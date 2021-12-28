44b093b3f82b5d5621bdd817418c62002a88d93a
mayra@mumuki.org <mayra@mumuki.org>
2021-12-28 16:47:08 +0000

    Mumuki Export on 2021-12-28 16:47:08 +0000

--- 00005_Help, I need somebody/description.md
+++ 00005_Help, I need somebody/description.md
@@ -1,1 +1,9 @@
-Algo algo
+El caballo cuando recibe rehabilitacion:
+* galopa 3  veces
+* come 3000 gramos de comida
+* galopa de nuevo 5 veces
+
+
+La golondrina:
+
+* Vuela hacia "Lihuel Calel" donda está el parque nacional 
--- 00005_Help, I need somebody/test.py
+++ 00005_Help, I need somebody/test.py
@@ -1,4 +1,21 @@
-class Test(unittest.TestCase):
-
-  def test_description_example(self):
-    self.assertTrue(True)
+  
+  def test_El_método_recibir_rehabilitacion_está_definido_en_la_clase_Caballo(self):
+    nano = Caballo(0,'Cuarto de Milla')
+    self.assertTrue("recibir_rehabilitacion" in dir(nano) and callable(nano.recibir_rehabilitacion))
+    
+  def test_Cuando_un_Caballo_recibe_rehabilitación_galopa_3_km,_come_3000_gramos_de_comida_y_vuelve_a_galopar_5_km(self):
+    nano = Caballo(1000,'Cuarto de Milla')
+    nano.recibir_rehabilitacion()
+    self.assertEqual(nano.energia,6992)
+    
+  
+  def test_El_método_recibir_rehabilitacion_está_definido_en_la_clase_Golondrina(self):
+    norita = Golondrina(0,'General las Heras')
+    self.assertTrue("recibir_rehabilitacion" in dir(norita) and callable(norita.recibir_rehabilitacion))
+    
+  def test_Cuando_una_golondrina_recibe_rehabilitación_vuela_hacia_Lihuel_Calel(self):
+    norita = Golondrina(100,'General Las Heras')
+    norita.recibir_rehabilitacion()
+    self.assertEqual(norita.ciudad, "Lihuel Calel")  
+    
+  
