299863f5b4fe22991c476c816f4b3d00a5e6b3aa
gustavo@mumuki.org <gustavo@mumuki.org>
2021-12-29 19:46:30 +0000

    Mumuki Export on 2021-12-29 19:46:30 +0000

--- 00008_Soooobreviviendoooo/test.py
+++ 00008_Soooobreviviendoooo/test.py
@@ -1,13 +1,13 @@
 
-  def test_Si_creo_un_nuevo_Zombi_le_puedo_especificar_su_adrenalina_inicial(self):
+  def test_Si_creo_un_nuevo_Zombi_le_puedo_especificar_su_hambre_inicial(self):
     zombi = Zombi(44)
-    self.assertEqual(zombi.adrenalina,44)
+    self.assertEqual(zombi.hambre,44)
     
 
-  def test_Si_creo_un_nuevo_SuperZombi_le_puedo_especificar_su_adrenalina_inicial(self):
+  def test_Si_creo_un_nuevo_SuperZombi_le_puedo_especificar_su_hambre_inicial(self):
     superZombi = SuperZombiZombi(44)
-    self.assertEqual(superZombi.adrenalina,44)
+    self.assertEqual(superZombi.hambre,44)
     
-  def test_Si_creo_un_nuevo_Sobreviviente_le_puedo_especificar_su_salud_inicial(self):
+  def test_Si_creo_un_nuevo_Sobreviviente_le_puedo_especificar_su_adrenalina_inicial(self):
     sobreviviente = Sobreviviente(10)
-    self.assertEqual(sobreviviente.salud,10)
+    self.assertEqual(sobreviviente.adrenalina,10)
--- 00009_Nuevo ejercicio/test.py
+++ 00009_Nuevo ejercicio/test.py
@@ -8,17 +8,17 @@
     burgess = Zombi(0)
     self.assertEqual(morfeo.adrenalina, 120)
     
-   def test_Si_un_sobreviviente_con_30_de_adrenalina_ataca_a_un_zombi_con_salud_inicial_40_su_salud_queda_en_25(self):
+   def test_Si_un_sobreviviente_con_30_de_adrenalina_ataca_a_un_zombi_con_hambre_inicial_40_su_hambre_queda_en_25(self):
     morfeo = Sobreviviente(30)
     burgess = Zombi(40)
-    self.assertEqual(burgess.salud, 25)
+    self.assertEqual(burgess.hambre, 25)
     
-   def test_Si_un_sobreviviente_con_100_de_adrenalina_ataca_a_un_zombi_con_salud_inicial_30_su_salud_queda_en_0(self):
+   def test_Si_un_sobreviviente_con_100_de_adrenalina_ataca_a_un_zombi_con_hambre_inicial_30_su_hambre_queda_en_0(self):
     morfeo = Sobreviviente(100)
     burgess = Zombi(30)
-    self.assertEqual(burgess.salud, 0)
+    self.assertEqual(burgess.hambre, 0)
     
-  # Que tire excepción cuando es un SuperZombi y cuando la salud de un zombi es > 500
+  # Que tire excepción cuando es un SuperZombi y cuando el hambre de un zombi es > 500
   
    def test_Un_super_zombi_sabe_correr(self):
     gaiman = SuperZombi(100)
@@ -27,10 +27,9 @@
    def test_Un_super_zombi_sabe_regenerarse(self):
     gaiman = SuperZombi(0)
     gaiman.regenerarse()
-    self.assertEqual(gaiman.salud, 100)
+    self.assertEqual(gaiman.hambre, 100)
     
-   def test_Si_un_super_zombi_con_salud_100_recibe_20_de_danio_su_salud_queda_en_40(self):
+   def test_Si_un_super_zombi_con_hambre_100_recibe_20_de_danio_su_hambre_queda_en_80(self):
     gaiman = SuperZombi(100)
     gaiman.recibir_danio(20)
-    self.assertEqual(gaiman.salud, 40)
-  
+    self.assertEqual(gaiman.hambre, 80)
