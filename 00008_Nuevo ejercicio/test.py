a9d22dfc49e15b20a82252702619c64fd9682354
mayra@mumuki.org <mayra@mumuki.org>
2021-12-29 14:24:45 +0000

    Mumuki Export on 2021-12-29 14:24:45 +0000

--- 00008_Nuevo ejercicio/test.py
+++ 00008_Nuevo ejercicio/test.py
@@ -1,4 +1,13 @@
-class Test(unittest.TestCase):
 
-  def test_description_example(self):
-    self.assertTrue(True)
+  def test_Si_creo_un_nuevo_Zombi_le_puedo_especificar_su_salud_inicial(self):
+    zombi = Zombi(44)
+    self.assertEqual(zombi.salud,44)
+    
+
+  def test_Si_creo_un_nuevo_SuperZombi_le_puedo_especificar_su_salud_inicial(self):
+    superZombi = SuperZombiZombi(44)
+    self.assertEqual(superZombi.salud,44)
+    
+  def test_Si_creo_un_nuevo_Sobreviviente_le_puedo_especificar_su_adrenalina_inicial(self):
+    sobreviviente = Sobreviviente(10)
+    self.assertEqual(sobreviviente.adrenalina,10)
