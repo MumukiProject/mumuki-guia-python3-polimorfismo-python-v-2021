0685c5b9b6d0e8cb9fb1e2ca89b271fe6e6a080e
mayra@mumuki.org <mayra@mumuki.org>
2021-12-30 13:43:10 +0000

    Mumuki Export on 2021-12-30 13:43:10 +0000

--- 00002_Sana sana/test.py
+++ 00002_Sana sana/test.py
@@ -1,6 +1,6 @@
   
   def test_El_método_rehabilitar_está_definido_en_la_clase_EstudianteDeVeterinaria(self):
-    agus = EstudianteVeterinaria()
+    agus = EstudianteDeVeterinaria()
     self.assertTrue("rehabilitar" in dir(agus) and callable(agus.rehabilitar))
     
   #VER COMO TESTEAR LA EXCEPCIÓN
