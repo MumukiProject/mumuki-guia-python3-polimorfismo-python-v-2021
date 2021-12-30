74319fab07df3483ffc3f0ccb72d9f665f191028
gustavo@mumuki.org <gustavo@mumuki.org>
2021-12-30 16:51:58 +0000

    Mumuki Export on 2021-12-30 16:51:58 +0000

--- 00002_Sana sana/test.py
+++ 00002_Sana sana/test.py
@@ -3,4 +3,9 @@
     agus = EstudianteDeVeterinaria()
     self.assertTrue("rehabilitar" in dir(agus) and callable(agus.rehabilitar))
     
+  def test_El_método_rehabilitar_hace_que_un_animal_reciba_rehabilitacion(self): 
+    morfeo = gato(100, 1000)
+    agus = EstudianteDeVeterinaria()
+    agus.rehabilitar(morfeo)
+    self.assertEqual(morfeo.energia, 1000)
   #VER COMO TESTEAR LA EXCEPCIÓN
