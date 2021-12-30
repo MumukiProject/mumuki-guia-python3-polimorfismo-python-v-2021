310efb101f247ea873a52756b59a8262fa79a585
gustavo@mumuki.org <gustavo@mumuki.org>
2021-12-30 17:00:34 +0000

    Mumuki Export on 2021-12-30 17:00:34 +0000

--- 00002_Sana sana/test.py
+++ 00002_Sana sana/test.py
@@ -4,7 +4,6 @@
     self.assertTrue("rehabilitar" in dir(agus) and callable(agus.rehabilitar))
     
   def test_El_método_rehabilitar_hace_que_un_animal_reciba_rehabilitacion(self): 
-    morfeo = Golondrina(100, "Las Heras")
+    morfeo = Gato(100, 1000)
     agus = EstudianteDeVeterinaria()
-    #self.assertRaises(AttributeError, agus.rehabilitar, morfeo)
     self.assertRaisesRegex(AttributeError, "'Gato' object has no attribute 'recibir_rehabilitacion'$", agus.rehabilitar, morfeo)
