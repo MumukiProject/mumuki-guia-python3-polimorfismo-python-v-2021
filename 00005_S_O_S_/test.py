290ba4fe8778049a18ccacba30b9dd50687d3e95
gustavo@mumuki.org <gustavo@mumuki.org>
2022-01-04 17:19:06 +0000

    Mumuki Export on 2022-01-04 17:19:06 +0000

--- 00005_S_O_S_/test.py
+++ 00005_S_O_S_/test.py
@@ -8,7 +8,6 @@
     nano.recibir_rehabilitacion()
     self.assertEqual(nano.energia,60992)
     
-  
   def test_El_método_recibir_rehabilitacion_está_definido_en_la_clase_Golondrina(self):
     norita = Golondrina(0,'General las Heras')
     self.assertTrue("recibir_rehabilitacion" in dir(norita) and callable(norita.recibir_rehabilitacion))
@@ -17,5 +16,3 @@
     norita = Golondrina(100,'General Las Heras')
     norita.recibir_rehabilitacion()
     self.assertEqual(norita.ciudad, "Lihuel Calel")  
-    
-  
