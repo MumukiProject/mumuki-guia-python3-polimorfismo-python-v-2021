3a950e9f6e51ec96a5ef71b053d30bfbf8963e67
mayra@mumuki.org <mayra@mumuki.org>
2022-01-04 17:57:50 +0000

    Mumuki Export on 2022-01-04 17:57:49 +0000

--- 00010_Hoy hagamos la excepción/test.py
+++ 00010_Hoy hagamos la excepción/test.py
@@ -1,4 +1,7 @@
-class Test(unittest.TestCase):
-
-  def test_description_example(self):
-    self.assertTrue(True)
+  
+  def test_Si_no_se_puede_atacar_a_un_zombi_porque_es_peligroso_se_lanza_la_excepcion_correspondiente(self):
+    hecate = Sobreviviente(100)
+    caliope = Zombi(100)
+    self.assertRaisesRegex(AttributeError, "'Sobreviviente' Es peligroso atacar a este zombi", hecate.atacar, caliope)
+    
+  #self.assertRaisesRegex(AttributeError, "'Gato' object has no attribute 'recibir_rehabilitacion'$", agus.rehabilitar, morfeo)
