180e1856f48ab096b564524f8e9269324c6cf05e
mayra@mumuki.org <mayra@mumuki.org>
2022-01-04 14:28:32 +0000

    Mumuki Export on 2022-01-04 14:28:32 +0000

--- 00008_No zombienvenidos/test.py
+++ 00008_No zombienvenidos/test.py
@@ -9,7 +9,7 @@
     
   def test_Un_super_zombi_sabe_correr(self):
     gaiman = SuperZombi(100)
-    self.assertTrue(milei.sabe_correr())
+    self.assertTrue(gaiman.sabe_correr())
     
   def test_Un_super_zombi_sabe_regenerarse(self):
     gaiman = SuperZombi(0)
@@ -20,3 +20,24 @@
     gaiman = SuperZombi(100)
     gaiman.recibir_danio(20)
     self.assertEqual(gaiman.hambre, 80)
+    
+  def test_Un_super_zombi_siempre_es_un_peligro(self):
+    gaiman = SuperZombi(0)
+    self.assertTrue(gaiman.es_un_peligro())
+    
+  def test_Un_zombi_sabe_correr(self):
+    gaiman = Zombi(100)
+    self.assertTrue(gaiman.sabe_correr())
+    
+  def test_Si_un_super_zombi_con_hambre_100_recibe_20_de_danio_su_hambre_queda_en_80(self):
+    gaiman = SuperZombi(100)
+    gaiman.recibir_danio(20)
+    self.assertEqual(gaiman.hambre, 80)
+    
+  def test_Un_Zombi_es_un_peligro_si_tiene_más_de_50_de_hambre(self):
+    gaiman = Zombi(50)
+    self.assertTrue(gaiman.es_un_peligro())
+    
+  def test_Un_Zombi_no_es_un_peligro_si_tiene_20_de_hambre(self):
+    gaiman = Zombi(20)
+    self.assertFalse(gaiman.es_un_peligro())
