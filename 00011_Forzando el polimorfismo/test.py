e46796bda33dcc8a6eacb320d7dfb5145ce91aa8
mayra@mumuki.org <mayra@mumuki.org>
2022-01-04 19:24:33 +0000

    Mumuki Export on 2022-01-04 19:24:33 +0000

--- 00011_Forzando el polimorfismo/test.py
+++ 00011_Forzando el polimorfismo/test.py
@@ -7,4 +7,10 @@
   def test_Una_planta_carnivora_zombi_es_un_peligro_si_tiene_mas_de_40_de_clorofila(self):
     mora = PlantaCarnivoraZombi(41)
     self.assertTrue(mora.es_un_peligro())
+    
+  def test_Una_planta_carnivora_zombi_al_recibir_danio_pierde_10_de_clorofila(self):
+    mora = PlantaCarnivoraZombi(30)
+    mora.recibir_danio(20)
+    self.assertEqual(mora.clorofila, 20)
+  
   
