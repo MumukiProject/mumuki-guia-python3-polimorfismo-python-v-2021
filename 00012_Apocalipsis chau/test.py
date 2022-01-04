b30758c00f5880aa5a2219d65488a468f410473e
mayra@mumuki.org <mayra@mumuki.org>
2022-01-04 19:38:33 +0000

    Mumuki Export on 2022-01-04 19:38:33 +0000

--- 00012_Apocalipsis chau/test.py
+++ 00012_Apocalipsis chau/test.py
@@ -1,4 +1,4 @@
-class Test(unittest.TestCase):
-
-  def test_description_example(self):
-    self.assertTrue(True)
+ 
+ def test_Una_planta_carnivora_zombi_es_un_peligro_si_tiene_mas_de_40_de_clorofila(self):
+    mora = PlantaCarnivoraZombi(41)
+    self.assertTrue(mora.es_un_peligro())
