bd02abc5eac2163d5d8939f07c4096a9973ac2db
mayra@mumuki.org <mayra@mumuki.org>
2022-01-04 19:31:54 +0000

    Mumuki Export on 2022-01-04 19:31:54 +0000

--- 00011_Forzando el polimorfismo/test.py
+++ 00011_Forzando el polimorfismo/test.py
@@ -1,11 +1,14 @@
-  
-  def test_Si_no_se_puede_atacar_a_un_zombi_porque_es_peligroso_se_lanza_la_excepcion_correspondiente(self):
-    hecate = Sobreviviente(100)
-    caliope = Zombi(100)
-    self.assertRaisesRegex(Exception, "Es peligroso atacar a este zombi", hecate.atacar, caliope)
-    
+
   def test_Una_planta_carnivora_zombi_es_un_peligro_si_tiene_mas_de_40_de_clorofila(self):
     mora = PlantaCarnivoraZombi(41)
+    self.assertTrue(mora.es_un_peligro())
+    
+  def test_Una_planta_carnivora_zombi_no_es_un_peligro_si_tiene_menos_de_40_de_clorofila(self):
+    mora = PlantaCarnivoraZombi(39)
+    self.assertFalse(mora.es_un_peligro())
+    
+  def test_Una_planta_carnivora_zombi_no_es_un_peligro_si_tiene_40_de_clorofila(self):
+    mora = PlantaCarnivoraZombi(40)
     self.assertTrue(mora.es_un_peligro())
     
   def test_Una_planta_carnivora_zombi_al_recibir_danio_pierde_10_de_clorofila(self):
