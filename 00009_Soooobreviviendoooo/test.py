e7cd14b4473b0ef8b6785081e8955b99c48e310e
mayra@mumuki.org <mayra@mumuki.org>
2022-01-04 15:22:24 +0000

    Mumuki Export on 2022-01-04 15:22:24 +0000

--- 00009_Soooobreviviendoooo/test.py
+++ 00009_Soooobreviviendoooo/test.py
@@ -2,21 +2,21 @@
     sobreviviente = Sobreviviente(10)
     self.assertEqual(sobreviviente.adrenalina,10)
     
-   def test_El_método_atacar_está_definido_en_la_clase_Sobreviviente(self):
+  def test_El_método_atacar_está_definido_en_la_clase_Sobreviviente(self):
     morfeo = Sobreviviente(100)
     self.assertTrue("atacar" in dir(morfeo) and callable(morfeo.atacar))
     
-   def test_Si_un_sobreviviente_ataca_a_un_zombi_que_no_es_peligroso_su_adrenalina_aumenta_en_20(self):
+  def test_Si_un_sobreviviente_ataca_a_un_zombi_que_no_es_peligroso_su_adrenalina_aumenta_en_20(self):
     morfeo = Sobreviviente(100)
     burgess = Zombi(0)
     self.assertEqual(morfeo.adrenalina, 120)
     
-   def test_Si_un_sobreviviente_con_30_de_adrenalina_ataca_a_un_zombi_con_hambre_inicial_40_su_hambre_queda_en_25(self):
+  def test_Si_un_sobreviviente_con_30_de_adrenalina_ataca_a_un_zombi_con_hambre_inicial_40_su_hambre_queda_en_25(self):
     morfeo = Sobreviviente(30)
     burgess = Zombi(40)
     self.assertEqual(burgess.hambre, 25)
     
-   def test_Si_un_sobreviviente_con_100_de_adrenalina_ataca_a_un_zombi_con_hambre_inicial_30_su_hambre_queda_en_0(self):
+  def test_Si_un_sobreviviente_con_100_de_adrenalina_ataca_a_un_zombi_con_hambre_inicial_30_su_hambre_queda_en_0(self):
     morfeo = Sobreviviente(100)
     burgess = Zombi(30)
     self.assertEqual(burgess.hambre, 0)
